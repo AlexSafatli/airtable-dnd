69c2c4854683ee532cbf1295c9f71967d6098234
Alex Safatli <safatli@cs.dal.ca>
2019-05-04 20:46:05 -0300

    Add more DM-specific character fields, allow family tree generation

--- entities/character.go
+++ entities/character.go
@@ -1,20 +1,27 @@
 package entities
 
 type Character struct {
-	Name       string
-	Race       string
-	Level      uint // == CR for Monsters
-	Class      string
-	HP         uint
-	AC         uint
-	Initiative int
-	PassivePer uint `json:"Passive Per"`
-	DexST      int  `json:"Dex ST"`
-	Size       CharacterSize
-	Type       string
-	Speed      uint8
-	Player     string
-	Affiliated bool
+	Name            string
+	Race            string
+	Level           uint // == CR for Monsters
+	Class           string
+	HP              uint
+	AC              uint
+	Initiative      int
+	PassivePer      uint `json:"Passive Per"`
+	DexST           int  `json:"Dex ST"`
+	Size            CharacterSize
+	Type            string
+	Speed           uint8
+	Player          string
+	Affiliated      bool
+	Gender          string // {Male, Female}
+	GenerationDelta int    `json:"Generation Delta"`
+	YOBDelta        int    `json:"Year of Birth Delta"`
+	YODDelta        int    `json:"Year of Death Delta"`
+	MotherID        uint   `json:"Mother ID"`
+	FatherID        uint   `json:"Father ID"`
+	LocationID      uint   `json:"Location ID"`
 }
 
 type CharacterSize uint8
