078f7709c08dee6af2a944a5578367d2800270bf
Alex Safatli <safatli@cs.dal.ca>
2019-05-10 15:46:46 -0300

    Add name conflict resolution for encounters

--- store/airtable.go
+++ store/airtable.go
@@ -44,10 +44,34 @@ func CreateCharacter(character entities.Character, tableName string, client *air
 	return record.AirtableID, err
 }
 
+func UpdateCharacter(id string, fields map[string]interface{}, tableName string, client *airtable.Client) error {
+	record := airtableCharacter{}
+	return client.UpdateRecord(tableName, id, fields, &record)
+}
+
+func GetEncounters(tableName string, client *airtable.Client) []entities.Encounter {
+	var records []airtableEncounter
+	if err := client.ListRecords(tableName, &records); err != nil {
+		return []entities.Encounter{}
+	}
+	var encounters []entities.Encounter
+	for i := range records {
+		if len(records[i].Fields.Name) > 0 {
+			encounters = append(encounters, records[i].Fields)
+		}
+	}
+	return encounters
+}
+
 func CreateEncounter(encounter entities.Encounter, tableName string, client *airtable.Client) (string, error) {
 	if len(encounter.Name) == 0 {
 		encounter.Name = fmt.Sprintf("s%d_l%d_r%d", encounter.Session,
 			encounter.Level, encounter.Room)
+	}
+	for i, e := range GetEncounters(tableName, client) {
+		if e.Name == encounter.Name {
+			encounter.Name = e.Name + fmt.Sprintf("_%d", i)
+		}
 	}
 	record := airtableEncounter{Fields: encounter}
 	err := client.CreateRecord(tableName, &record)
